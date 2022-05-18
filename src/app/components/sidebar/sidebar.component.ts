c2fbfe0ff85ee026da8a7dc23e846304265955d0
Lorena Alfonso <jlalfonso@itc.com.co>
2022-05-18 01:06:18 -0500

    frontend alerta temprana

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Inicio',  icon: 'dashboard', class: '' },
+    { path: '/user-profile', title: 'Registrar habitante',  icon:'person', class: '' },
+    { path: '/table-list', title: 'Habitantes',  icon:'listalt', class: '' },
+    { path: '/registro', title: 'Registrar evento',  icon:'bubble_chart', class: '' },
+    //{ path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
+    { path: '/icons', title: 'Informes',  icon:'content_paste', class: '' },
+    { path: '/maps', title: 'Ubicación',  icon:'location_on', class: '' },
+    { path: '/notifications', title: 'Mapas',  icon:'coronavirus', class: '' }
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
